128eb4e6545a86b516980fb109175a82629b647f
olaoluwa ayanbola <olaoluwaayanbola@gmail.com>
2025-11-28 17:55:40 +0100

    feat:fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { copyFileSync } from 'fs'
-import { join } from 'path'
-import { readFileSync } from 'fs'
+// import { copyFileSync } from 'fs'
+//  import { join } from 'path'
+// import { readFileSync } from 'fs'
 
 // Read the repository name from the config file
-const repoConfig = readFileSync(join(__dirname, 'src', 'config', 'repo.ts'), 'utf-8')
-const repoMatch = repoConfig.match(/export const REPO_NAME = ['"](.+?)['"]/)
-const REPO_NAME = repoMatch ? repoMatch[1] : 'admin-v3'
+// const repoConfig = readFileSync(join(__dirname, 'src', 'config', 'repo.ts'), 'utf-8')
+// const repoMatch = repoConfig.match(/export const REPO_NAME = ['"](.+?)['"]/)
+// const REPO_NAME = repoMatch ? repoMatch[1] : 'admin-v3'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    {
-      name: 'copy-404',
-      closeBundle() {
-        // Copy index.html to 404.html for GitHub Pages SPA routing
-        copyFileSync(
-          join(__dirname, 'dist', 'index.html'),
-          join(__dirname, 'dist', '404.html')
-        )
-      },
-    },
+    // {
+    //   name: 'copy-404',
+    //   closeBundle() {
+    //     // Copy index.html to 404.html for GitHub Pages SPA routing
+    //     copyFileSync(
+    //       join(__dirname, 'dist', 'index.html'),
+    //       join(__dirname, 'dist', '404.html')
+    //     )
+    //   },
+    // },
   ],
-  base: `/${REPO_NAME}/`,
+  build:{
+    outDir: 'dist',
+    // base: '/care-plan-365/adminv3/',
+  }
+  // base: `/${REPO_NAME}/`,
 })
